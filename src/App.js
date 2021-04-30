e23b1a460d1c6d2e9aa37f4992efbbf00d5d4a34
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2021-04-29 22:03:13 -0700

    slight modification to assets, and attempt at Nav component. testing still in progress

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 //components to import
 // import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
-// import Header from "./components/Header";
+import Header from "./components/Header";
 // import ContactForm from "./components/Contact";
 
 
@@ -10,6 +10,7 @@ function App() {
   return (
     //jsx
     <div>
+      <Header />
     <main>
         <p>MY PORTFOLIO IS IN PROGRESS...hang tight</p>
     </main>
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Nav(props) {
+
+  
+    return (
+        <nav className='navbar'>
+            <h1 className='name'>
+                Erika Salcedo
+            </h1>
+            <ul className='flex space-a'>
+               
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
