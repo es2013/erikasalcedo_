fb26e3e7ca12df5b954a301e542476f732bc9756
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2021-05-02 11:25:15 -0700

    fixed read me

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -36,7 +36,9 @@ function Header() {
         </Jumbotron>
         <Container class="shadow mb-5 bg-light">
           <h2>A little bit about me</h2>
-          <p>If you were to ask me which animal/insect best represents me, I would say a spider because I am a web developer that enjoys catching bugs. (Okay maybe not when there is a time sensitive project because bugs can drive me crazy) but I do enjoy troubleshooting. I am a full stack developer based out of California with past experience in marketing automation and data analysis. </p>
+          <p>If you were to ask me which animal/insect best represents me, I would say a spider because I am a web developer that enjoys catching bugs. 
+            (Okay maybe not when there is a time sensitive project because bugs can drive me crazy) but I do enjoy troubleshooting. 
+            I earned a full stack development certificate from the University of California, Los Angeles Coding Bootcamp in 2021. Most of the projects I have worked with technologies ranging from JavaScript, HTML, CSS, to Express.js, React, MongoDB, and MySQL. I have past experience in marketing automation and data analysis. When I am not behind a computer screen, you can find be at the park with my little 7-year old chi-poodle-terrier named Penny. </p>
         </Container>
     </section>
   );
